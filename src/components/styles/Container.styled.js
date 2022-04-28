711520ae9c92ac560b7ceaf9467eeaaf94fd4ef0
PaulPintang <pauljpintang@gmail.com>
2022-04-28 21:44:51 +0800

    style: add styled container and flex

---
+++ src/components/styles/Container.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1100px;
+  margin: auto;
+  padding: 0 25px;
+`;
---
+++ src/components/styles/Flex.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Flex = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
