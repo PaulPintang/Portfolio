87c276c193d96f802683fca1328a2eceef7657f5
PaulPintang <pauljpintang@gmail.com>
2022-04-29 15:53:16 +0800

    style: projects, title styles

--- src/components/Projects/Projects.styled.js
+++ src/components/Projects/Projects.styled.js
@@ -1,110 +1,22 @@
 import styled from "styled-components";
-
-export const ProjectStyled = styled.div`
-  h2 {
-    text-align: center;
-    padding-bottom: 40px;
-  }
-  h3 {
-    font-size: 25px;
-  }
-  h4 {
-    display: flex;
-    justify-items: flex-end;
-  }
-  small {
-    display: block;
-  }
-  .img-flex {
-    display: flex;
-    justify-content: center;
-    width: 900px;
-    margin: auto;
-    position: relative;
-
-    img {
-      transition: transform 0.2s ease-in;
-      &:hover {
-        cursor: pointer;
-        position: relative;
-        z-index: 1;
-        transform: translateY(-9px);
-      }
-    }
-
-    img-info {
-      display: flex;
-      align-items: flex-end;
-      flex-direction: column;
-      position: relative;
-      right: 180px;
-      padding: 50px 0;
-      height: 200px;
-
-      .flex {
-        gap: 10px;
-        padding: 8px 0;
-      }
-      small {
-        display: block;
-        font-size: 13px;
-      }
-    }
-    .img-info-2 {
-      display: flex;
-      align-items: flex-start;
-      flex-direction: column;
-      position: relative;
-      left: 180px;
-      padding: 50px 0;
-      height: 200px;
-
-      .flex {
-        gap: 10px;
-        padding: 8px 0;
-      }
-      .opt {
-        margin-left: 14px;
-        a {
-          margin-left: -14px;
-          font-size: 18px;
-          &:hover {
-            cursor: pointer;
-            color: #fd0037;
-          }
-        }
-      }
-    }
-  }
-  
-  }
-  .img-desc {
-    width: 450px;
-    height: auto;
-    background-color: #202a3c;
-    box-shadow: 0 0px 5px 1px hsl(217deg 27% 15%);
-    padding: 15px;
-    margin: 15px 0;
-    a {
-      font-size: 10px;
-      font-variant: normal;
-      color: unset;
-      font-size: unset;
-      font-weight: unset;
-      font-variant: unset;
-      letter-spacing: unset;
-      padding-bottom: unset;
-      font-style: italic;
-      color: #fd0037;
-      border-bottom: 0.1px solid #fd0037;
-      padding-bottom: 1px;
-      margin-left: 5px;
-    }
-    p {
-      opacity: 1;
-    }
-`;
+import { Flex } from "../styles/Reusable.styled";
 
 export const Section = styled.section`
   padding-bottom: 70px;
+  h2 {
+    padding-bottom: 40px;
+  }
 `;
+
+export const ProjectContainer = styled(Flex)``;
+export const ProjectInfo = styled.div`
+  div {
+    text-align: right;
+    h3 {
+      font-size: 25px;
+      font-weight: bold;
+    }
+  }
+`;
+export const Description = styled.div``;
+export const LinkToProject = styled.div``;
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,6 +1,12 @@
 // import "./styles.css";
-import { Container, Span } from "../styles/Reusable.styled.js";
-import { Section } from "./Projects.styled";
+import { Flex, Container, Span, Links } from "../styles/Reusable.styled.js";
+import {
+  Section,
+  ProjectContainer,
+  ProjectInfo,
+  Description,
+  LinkToProject,
+} from "./Projects.styled";
 import img1 from "../../images/projects/11.png";
 import img2 from "../../images/projects/22.png";
 import img3 from "../../images/projects/33.png";
@@ -13,7 +19,7 @@ const Projects = () => {
         <h2>
           My <Span>Projects</Span>
         </h2>
-        <div className="img-flex reveal">
+        <ProjectContainer className="rev eal">
           <div>
             <a
               href="https://paulpintang.github.io/Covid-19-Tracker-App/"
@@ -23,15 +29,15 @@ const Projects = () => {
               <img src={img1} alt="" />
             </a>
           </div>
-          <div className="img-info">
-            <div className="img-title">
+          <ProjectInfo>
+            <div>
               <h4>
-                No.<span>01</span>
+                No.<Span>01</Span>
               </h4>
               <h3>Covid-19 Tracker</h3>
             </div>
 
-            <div className="img-desc">
+            <Description>
               <p>
                 This is my first project while learning javascript. I made this
                 Covid-19 Tracker app after learning how to use an API on a
@@ -41,17 +47,17 @@ const Projects = () => {
                   Covid-19 API
                 </a>
               </p>
-            </div>
+            </Description>
 
-            <div className="flex">
+            <Flex>
               <small>HTML</small>
               <small>CSS</small>
               <small>JAVASCRIPT</small>
               <small>Covid-19 API</small>
-            </div>
+            </Flex>
 
-            <div className="opt">
-              <ul>
+            <LinkToProject>
+              <Links>
                 <li>
                   <a
                     href="https://github.com/PaulPintang/Covid-19-Tracker-App"
@@ -70,204 +76,10 @@ const Projects = () => {
                     <i className="fas fa-external-link-alt"></i>
                   </a>
                 </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-        <div className="img-flex reveal">
-          <div className="img-info-2">
-            <div className="img-title">
-              <h4>
-                No.<span>02</span>
-              </h4>
-              <h3>Fylo Landing Page</h3>
-            </div>
-
-            <div className="img-desc">
-              <p>
-                This is a frontendmentor.io landing page challenge to help me
-                practice creating responsive websites. This task was quite
-                beneficial in terms of learning CSS best practices. Here's the
-                link to the challenge from their website:
-                <a
-                  href="https://www.frontendmentor.io/challenges/fylo-dark-theme-landing-page-5ca5f2d21e82137ec91a50fd"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  fylo-landing-page-challenge
-                </a>
-              </p>
-            </div>
-
-            <div className="flex">
-              <small>HTML</small>
-              <small>CSS</small>
-              <small>JAVASCRIPT</small>
-            </div>
-
-            <div className="opt">
-              <ul>
-                <li>
-                  <a
-                    href="https://github.com/PaulPintang/Fylo-Landing-page"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fab fa-github"></i>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://festive-goldberg-678aaa.netlify.app/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fas fa-external-link-alt"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div>
-            <a
-              href="https://festive-goldberg-678aaa.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={img2} alt="" />
-            </a>
-          </div>
-        </div>
-
-        <div className="img-flex reveal">
-          <div>
-            <a
-              href="https://sharp-heyrovsky-44e00e.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={img3} alt="" />
-            </a>
-          </div>
-          <div className="img-info">
-            <div className="img-title">
-              <h4>
-                No.<span>03</span>
-              </h4>
-              <h3>EasyBank Landing Page</h3>
-            </div>
-
-            <div className="img-desc">
-              <p>
-                The frontendmentor.io website serves as a classroom setting for
-                me to improve my development skills. This project challenged me
-                to design the navigation bar from scratch. Here's the link to
-                the challenge from their website.
-                <a
-                  href="https://www.frontendmentor.io/challenges/easybank-landing-page-WaUhkoDN"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  easybank-landing-page-challenge
-                </a>
-              </p>
-            </div>
-
-            <div className="flex">
-              <small>HTML</small>
-              <small>CSS</small>
-              <small>JAVASCRIPT</small>
-            </div>
-
-            <div className="opt">
-              <ul>
-                <li>
-                  <a
-                    href="https://github.com/PaulPintang/easybank-landing-page"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fab fa-github"></i>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://sharp-heyrovsky-44e00e.netlify.app/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fas fa-external-link-alt"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="img-flex reveal">
-          <div className="img-info-2">
-            <div className="img-title">
-              <h4>
-                No.<span>04</span>
-              </h4>
-              <h3>Social Media Dashboard</h3>
-            </div>
-
-            <div className="img-desc">
-              <p>
-                This frontendmentor.io challenge put me to a test in javascript.
-                This simple project improved my understanding of DOM
-                manipulation and javascript event listerner implementation.
-                Here's the link to the challenge from their website.
-                <a
-                  href="https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  social-media-dashboard-challenge
-                </a>
-              </p>
-            </div>
-
-            <div className="flex">
-              <small>HTML</small>
-              <small>CSS</small>
-              <small>JAVASCRIPT</small>
-            </div>
-
-            <div className="opt">
-              <ul>
-                <li>
-                  <a
-                    href="https://github.com/PaulPintang/Social-media-dashboard"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fab fa-github"></i>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://socialllll-media-dashboard.netlify.app/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fas fa-external-link-alt"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="mt">
-            <a
-              href="https://socialllll-media-dashboard.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={img4} alt="" />
-            </a>
-          </div>
-        </div>
+              </Links>
+            </LinkToProject>
+          </ProjectInfo>
+        </ProjectContainer>
       </Container>
     </Section>
   );
