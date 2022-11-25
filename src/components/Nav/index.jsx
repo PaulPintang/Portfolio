a10752b5c6698c013a8d3779e43d440107de7db2
PaulPintang <pauljpintang@gmail.com>
2022-11-25 15:11:58 +0800

    update resume typo

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -145,7 +145,7 @@ const Nav = () => {
 
           <li>
             <a
-              href="https://drive.google.com/file/d/11pdYyj6SzOhSCt3Ehi1K_mEVj5bdHN3B/view?usp=share_link"
+              href="https://drive.google.com/file/d/1pLsFkeB-V6-DOSvAG_8IpJU01t9_4rbx/view?usp=sharing"
               target="_blank"
               rel="noreferrer"
             >
