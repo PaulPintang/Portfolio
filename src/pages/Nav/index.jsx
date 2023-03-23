6d6189b915552567143845bfbb488b0a42ed67f8
PaulPintang <pauljpintang@gmail.com>
2023-03-23 10:48:53 +0800

    resume

--- src/pages/Nav/index.jsx
+++ src/pages/Nav/index.jsx
@@ -151,7 +151,7 @@ const Nav = () => {
 
           <li>
             <a
-              href="https://drive.google.com/file/d/1mw4O5rsCLKlfciHPfkHoe1Qc4ijSlUMH/view?usp=sharing"
+              href="https://paulpintang.com/resume.pdf"
               target="_blank"
               rel="noreferrer"
             >
