f0e1ea7f7a0a502d3d289c2b879de6f62f52558a
PaulPintang <pauljpintang@gmail.com>
2023-03-28 09:14:58 +0800

    fix: deleted import

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Skills from "./pages/Skills";
 import Projects from "./pages/Projects";
 import Contacts from "./pages/Contacts";
 import GlobalStyles from "./pages/styles/GlobalStyles";
+import Experience from "./pages/Experience";
 
 function App() {
   return (
