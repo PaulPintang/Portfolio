10ee7cbc30fa516a99f08ee16acd12212a44e819
Paul Pintang <paulpintang.dev@gmail.com>
2026-01-26 11:48:06 +0800

    update offer page

--- src/pages/Offer/index.jsx
+++ src/pages/Offer/index.jsx
@@ -7,57 +7,57 @@ import design from "../../assets/svg/design.svg";
 import team from "../../assets/svg/team.svg";
 
 const Offer = () => {
-  const { Reveal, revealAnimation } = useContext(AnimationContext);
-  return (
-    <Container id="services">
-      <Section>
-        <Center>
-          <h2>
-            What i <Span>offer?</Span>
-          </h2>
-          <p>Create positive user experience</p>
-        </Center>
-        <Flex justify>
-          <Reveal keyframes={revealAnimation}>
-            <OfferCards>
-              <Center>
-                <img src={responsive} alt="" />
-                <h3>Responsive Website</h3>
-                <p>
-                  Develop a responsive website layout for a better user
-                  experience
-                </p>
-              </Center>
-            </OfferCards>
-          </Reveal>
-          <Reveal keyframes={revealAnimation}>
-            <OfferCards>
-              <Center>
-                <img src={design} alt="" />
-                <h3>Convert your design into a working website.</h3>
-                <p>
-                  Make an ideal functional website with your mockups and UI
-                  prototypes.
-                </p>
-              </Center>
-            </OfferCards>
-          </Reveal>
-          <Reveal keyframes={revealAnimation}>
-            <OfferCards>
-              <Center>
-                <img src={team} alt="" />
-                <h3>Team Collaboration</h3>
-                <p>
-                  Can work as part of a team and has outstanding collaboration
-                  skills.
-                </p>
-              </Center>
-            </OfferCards>
-          </Reveal>
-        </Flex>
-      </Section>
-    </Container>
-  );
+	const { Reveal, revealAnimation } = useContext(AnimationContext);
+	return (
+		<Container id="services">
+			<Section>
+				<Center>
+					<h2>
+						What I <Span>Offer</Span>
+					</h2>
+					<p>Building complete, scalable web applications</p>
+				</Center>
+				<Flex justify>
+					<Reveal keyframes={revealAnimation}>
+						<OfferCards>
+							<Center>
+								<img src={responsive} alt="" />
+								<h3>Full-Stack Web Applications</h3>
+								<p>
+									Implement complete web applications including frontend logic,
+									backend APIs, and database integration.
+								</p>
+							</Center>
+						</OfferCards>
+					</Reveal>
+					<Reveal keyframes={revealAnimation}>
+						<OfferCards>
+							<Center>
+								<img src={design} alt="" />
+								<h3>Frontend & Backend Integration</h3>
+								<p>
+									Convert UI designs into fully functional systems with APIs,
+									authentication, and database integration.
+								</p>
+							</Center>
+						</OfferCards>
+					</Reveal>
+					<Reveal keyframes={revealAnimation}>
+						<OfferCards>
+							<Center>
+								<img src={team} alt="" />
+								<h3>Scalable Solutions</h3>
+								<p>
+									Build clean, maintainable, and scalable applications that are
+									easy to extend and collaborate on.
+								</p>
+							</Center>
+						</OfferCards>
+					</Reveal>
+				</Flex>
+			</Section>
+		</Container>
+	);
 };
 
 export default Offer;
